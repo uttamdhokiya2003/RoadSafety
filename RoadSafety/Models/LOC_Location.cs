6353cabf39b23eadadbb4677dbeca8eac19cf52d
uttamdhokiya2003 <uttamhokiya84@gmail.com>
2023-02-25 23:57:49 +0530

    s

--- RoadSafety/Models/LOC_Location.cs
+++ RoadSafety/Models/LOC_Location.cs
@@ -2,5 +2,18 @@
 {
 	public class LOC_Location
 	{
+		public int? LocationID { get; set; }
+
+		public int? CountryID { get; set; }
+		public int? CityID { get; set; }
+
+		public int? StateID { get; set; }
+
+		public string? Address { get; set; }
+
+		public string? Longitude { get; set; }
+
+		public string? Latitude { get; set; }
+
 	}
 }

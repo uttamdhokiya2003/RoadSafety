e25bb3e8c23fa692c7cbc081fb392f9074e0315d
uttamdhokiya2003 <uttamhokiya84@gmail.com>
2023-03-01 19:37:24 +0530

    s

---
+++ RoadSafety/Controllers/MapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoadSafety.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("map");
+        }
+    }
+}
